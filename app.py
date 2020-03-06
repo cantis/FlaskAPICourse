38affeea7db02aafd9a40fa3b3c8f0e6b98d7f08
Evan <eyoung@winnipeg.ca>
2020-03-06 16:46:54 -0600

    End of lecture 61

--- app.py
+++ app.py
@@ -1,18 +1,41 @@
 from flask import Flask
 
 app = Flask(__name__)
+stores = [
+    {
+        'name': 'My Wonderful Store',
+        'items': [
+            {
+                'name': 'My Item',
+                'price': 15.99
+            }
+        ]
+    }
+]
 
 # POST /store {name:}
-@app.route('/store')
+@app.route('/store', methods=['POST'])
+def create_store():
+    pass
 
 # GET /store/<string:name>
+@app.route('/store/<string:name>')
+def get_store():
+    pass
+
 # GET /store
+@app.route('/store')
+def get_stores():
+    pass
+
 # POST /store/<string:name>/item {name:, price:}
+@app.route('/store/<string:name>/item/', methods=['POST'])
+def create_item_in_store():
+    pass
+
 # GET /store/<string:name>/item
-
-
-
-
-
+@app.route('/store/<string:name>/item')
+def get_items_in_store():
+    pass
 
 app.run(port=5001)
